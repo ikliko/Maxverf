a30a17cb718f272666946d09832019b6d0a856a3
RadoslavRadkov <RadoslavRadkov>
2016-01-12 14:22:21 +0200

    Created form that asks user for role when try to order

--- resources/views/payments/role.blade.php
+++ resources/views/payments/role.blade.php
@@ -1,4 +1,17 @@
 @extends('master')
 @section('container')
-<h2>SELECT ROLE FORM</h2>
+    <div class="container margin-top">
+        <div id="sing_form">
+            <h2>SELECT ROLE FORM</h2>
+            {!! Form::open(['url' => 'payments/method/role']) !!}
+                <div id="top">
+                    <button class="btn btn-primary" name="role" value="newUser">Register</button>
+                    <span>or</span>
+                </div>
+                <div id="bottom">
+                    <button class="btn btn-primary" name="role" value="guest">Order like guest</button>
+                </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
